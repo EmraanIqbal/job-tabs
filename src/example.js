811cf3d110757dade5fb4372ce4ba0a3d1f9cec8
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-10 06:11:37 -0700

    example.js

--- src/example.js
+++ src/example.js
@@ -3,11 +3,11 @@ import { useColorMode } from "@chakra-ui/color-mode"
 import { MoonIcon } from '@chakra-ui/icons'
 import React from 'react'
 export default function Example() {
-    const { colorMode, toggleColorMode } = useColorMode()
+    const { toggleColorMode } = useColorMode()
     return (
       <header>
-        <Button mt='-70' float='right' onClick={toggleColorMode}>
-          <MoonIcon/> {colorMode === "light" ? "Dark" : "Light"}
+        <Button mt='-50' mr='20' float='right' onClick={toggleColorMode}>
+          <MoonIcon/> 
         </Button>
       </header>
     )
