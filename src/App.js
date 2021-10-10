c6a8c868a97efd67fcd2787430135df7cea44a68
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-10 01:54:52 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Tab from "./components/Tab";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey React</h1>
+      <Tab />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tab.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const url = "https://course-api.com/react-tabs-project";
+const Tab = () => {
+  return (
+    <div>
+      <h1>This is job tab component</h1>
+    </div>
+  );
+};
+
+export default Tab;
