ffc75e79d796f4aa2b470ec368e53ff26de660cb
MImranIqbal <se.imraniqbal@gmail.com>
2021-11-12 01:19:18 +0500

    src

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,6 @@ import { extendTheme } from "@chakra-ui/react";
 const config = {
   initialColorMode: "light",
   useSystemColorMode: false,
-  d,
 };
 const theme = extendTheme({ config });
 export default theme;
